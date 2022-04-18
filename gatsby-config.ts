de4f8ab966ab1e94d35fc4098b22921172d9aa5c
Dan Stafford <danstafford5@gmail.com>
2022-04-18 18:02:04 -0400

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `gatsby-drupal-test`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
